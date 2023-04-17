9ba0745258ee7ba9766e3cc87008dca2cb2f86f6
Ramez Taher <ramezgamer11@gmail.com>
2023-04-18 00:19:17 +0100

    done for subject route

--- routes/subject.routes.js
+++ routes/subject.routes.js
@@ -1,9 +1,22 @@
 import express from "express"
-import { createSubject } from "../controllers/subject.controllers.js"
+import {
+  createSubject,
+  deleteSubject,
+  updateSubject,
+} from "../controllers/subject.controllers.js"
 import { admin, protect } from "../middlewares/auth.middlewares.js"
 
 const router = express.Router()
 
-router.route("/").post(protect, admin, createSubject)
+router
+  .route("/")
+  .post(protect, admin, createSubject)
+  .get(protect, createSubject)
+
+router
+  .route("/:id")
+  .get(protect, createSubject)
+  .delete(protect, admin, deleteSubject)
+  .put(protect, admin, updateSubject)
 
 export default router

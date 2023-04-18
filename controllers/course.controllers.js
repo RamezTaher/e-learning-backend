60fb722210ac4251d94f48378910ad435737ea9b
Ramez Taher <ramezgamer11@gmail.com>
2023-04-18 17:45:11 +0100

    add full course endpoint

--- controllers/course.controllers.js
+++ controllers/course.controllers.js
@@ -12,4 +12,58 @@ const createCourse = asyncHandler(async (req, res) => {
   }
 })
 
-export { createCourse }
+const getCourses = asyncHandler(async (req, res) => {
+  const courses = await Course.find()
+  res.json(courses)
+})
+
+const getCourseById = asyncHandler(async (req, res) => {
+  const course = await Course.findById(req.params.id)
+
+  if (course) {
+    res.json(course)
+  } else {
+    res.status(404)
+    throw new Error("Course not found")
+  }
+})
+
+const updateCourse = asyncHandler(async (req, res) => {
+  const course = await Course.findById(req.params.id)
+
+  if (course) {
+    course.title = req.body.title || course.title
+    course.description = req.body.description || course.description
+    course.image = req.body.image || course.image
+    course.level = req.body.level || course.level
+    course.startDate = req.body.startDate || course.startDate
+
+    await course.save()
+    res.status(200).json({
+      message: "Course Updated Successfully",
+      code: 200,
+      success: true,
+    })
+  } else {
+    res.status(404)
+    throw new Error("Course not found")
+  }
+})
+
+const deleteCourse = asyncHandler(async (req, res) => {
+  const course = await Course.findById(req.params.id)
+
+  if (course) {
+    await course.deleteOne()
+    res.json({
+      message: "Course Removed successfully",
+      code: 200,
+      success: true,
+    })
+  } else {
+    res.status(404)
+    throw new Error("Course not found")
+  }
+})
+
+export { createCourse, getCourses, getCourseById, updateCourse, deleteCourse }
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -1,9 +1,24 @@
 import express from "express"
-
 import { protect, admin } from "../middlewares/auth.middlewares.js"
-import { createCourse } from "../controllers/course.controllers.js"
+
+import {
+  createCourse,
+  deleteCourse,
+  getCourseById,
+  getCourses,
+  updateCourse,
+} from "../controllers/course.controllers.js"
 const router = express.Router()
 
-router.route("/").post(protect, admin, createCourse)
+router
+  .route("/")
+  .post(protect, admin, createCourse)
+  .get(protect, admin, getCourses)
+
+router
+  .route("/:id")
+  .get(protect, getCourseById)
+  .delete(protect, admin, deleteCourse)
+  .put(protect, admin, updateCourse)
 
 export default router

a41470cce29748afefe4d5f859b03ec0cde9c452
Ramez Taher <ramezgamer11@gmail.com>
2023-05-14 11:47:02 +0100

    added something

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,9 +1,6 @@
 import jwt from 'jsonwebtoken'
 import User from '../models/user.models.js'
 import asyncHandler from 'express-async-handler'
-
-
-
 const protect = asyncHandler(async (req, res, next) => {
   let token
 
